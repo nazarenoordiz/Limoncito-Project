e404cc2d84aee1ca1e146ba3a8372b8c59749c13
Nazareno Ordiz <nazarenoe.ordiz@gmail.com>
2023-07-14 18:42:19 -0300

    limoncito first commit

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+from typing import Callable
+
+import openai
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+APP_TOKEN = "xapp-1-A05C7A7GFG8-5405177267601-3c3f8bc0a209a1e55f3532c3efb9e6462b956d24542d5f7f215bd04aee737fa5"
+BOT_TOKEN = "xoxb-2682507849171-5391524871747-bRxKDdtW4U2kGfCynDyVMiyU"
+COMPLETIONS_MODEL = "text-davinci-002"
+COMPLETIONS_API_PARAMS = {
+    "temperature": 0.0,
+    "max_tokens": 300,
+    "model": COMPLETIONS_MODEL,
+}
+app = App(token=BOT_TOKEN)
+
+
+@app.event("app_mention")
+def mention_handler(body: dict, say: Callable):
+    import pdb; pdb.set_trace()
+    sender_id = f"<@{body.get('event', {}).get('user')}>"
+    bot_id = body.get("event", {}).get("text").split()[0]
+    message = body.get("event", {}).get("text")
+    message = message.replace(bot_id, "").strip()
+    if message:
+        answer = get_answer(message)
+        say(f"Hey {sender_id}: {answer}")
+
+def get_answer(message: str):
+    # response = openai.Completion.create(prompt=message, **COMPLETIONS_API_PARAMS)
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=message,
+        temperature=0.5,
+        max_tokens=256,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0
+    )
+    import pdb; pdb.set_trace()
+    return response["choices"][0]["text"].strip(" \n")
+
+openai.api_key = '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'
+
+
+
+
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+
+
+if __name__ == "__main__":
+    handler = SocketModeHandler(app, APP_TOKEN)
+    handler.start()
