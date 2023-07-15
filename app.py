71155f2b7e7454d3e71153b9fe99d237e1f5625a
nazarenoordiz <nazarenoe.ordiz@gmail.com>
2023-07-14 21:57:59 -0300

    reforming the app.py

--- app.py
+++ app.py
@@ -4,55 +4,52 @@ from typing import Callable
 import openai
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
 
-APP_TOKEN = "xapp-1-A05C7A7GFG8-5405177267601-..."
-BOT_TOKEN = "xoxb-268250784917-..."
-COMPLETIONS_MODEL = "text-davinci-002"
-COMPLETIONS_API_PARAMS = {
-    "temperature": 0.0,
-    "max_tokens": 300,
-    "model": COMPLETIONS_MODEL,
-}
-app = App(token=BOT_TOKEN)
+load_dotenv()
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
+class Limoncito:
+    SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN")
+    SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN")
+    COMPLETIONS_MODEL = "text-davinci-002"
+    COMPLETIONS_API_PARAMS = {
+        "temperature": 0.0,
+        "max_tokens": 300,
+        "model": COMPLETIONS_MODEL,
+    }
+    app = App(token=SLACK_BOT_TOKEN)
+    
+    def __init__(self):
+        pass
 
-@app.event("app_mention")
-def mention_handler(body: dict, say: Callable):
-    import pdb; pdb.set_trace()
-    sender_id = f"<@{body.get('event', {}).get('user')}>"
-    bot_id = body.get("event", {}).get("text").split()[0]
-    message = body.get("event", {}).get("text")
-    message = message.replace(bot_id, "").strip()
-    if message:
-        answer = get_answer(message)
-        say(f"Hey {sender_id}: {answer}")
+    @app.event("app_mention")
+    def mention_handler(self, body: dict, say: Callable):
+        sender_id = f"<@{body.get('event', {}).get('user')}>"
+        bot_id = body.get("event", {}).get("text").split()[0]
+        message = body.get("event", {}).get("text")
+        message = message.replace(bot_id, "").strip()
+        if message:
+            answer = self.get_answer(message)
+            say(f"Hey {sender_id}: {answer}")
 
-def get_answer(message: str):
-    # response = openai.Completion.create(prompt=message, **COMPLETIONS_API_PARAMS)
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=message,
-        temperature=0.5,
-        max_tokens=256,
-        top_p=1.0,
-        frequency_penalty=0.0,
-        presence_penalty=0.0
-    )
-    import pdb; pdb.set_trace()
-    return response["choices"][0]["text"].strip(" \n")
+    def get_answer(self, message: str) -> str:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=message,
+            temperature=0.5,
+            max_tokens=256,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
+        )
+        return response["choices"][0]["text"].strip(" \n")
 
-openai.api_key = 'sk-xmBLbTO6yOhJz...'
+    @app.message("hello")
+    def message_hello(self, message: str, say: Callable) -> str:
+        # say() sends a message to the channel where the event was triggered
+        say(f"Hey there <@{message['user']}>!")
 
-
-
-
-@app.message("hello")
-def message_hello(message, say):
-    # say() sends a message to the channel where the event was triggered
-    say(f"Hey there <@{message['user']}>!")
-
-
-
-if __name__ == "__main__":
-    handler = SocketModeHandler(app, APP_TOKEN)
-    handler.start()
+    if __name__ == "__main__":
+        handler = SocketModeHandler(app, SLACK_APP_TOKEN)
+        handler.start()
